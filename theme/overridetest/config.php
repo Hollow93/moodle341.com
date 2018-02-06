44529317ae5caedffb83bccf4eefd51c427782f0
Владислав Львович Быконь <Hollow-oren@yandex.ru>
2018-02-06 22:12:54 +0300

    overraid base on documentation

---
+++ theme/overridetest/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$THEME->name = 'overridetest';
+$THEME->parents = array('standard', 'base', 'aardvark');
+
+$THEME->rendererfactory = 'theme_overridden_renderer_factory';
+
+$THEME->sheets = Array('overridetest');
+
