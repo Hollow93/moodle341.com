c6cf9fdbf981c7192973925b786a0674bb2e3383
Владислав Львович Быконь <Hollow-oren@yandex.ru>
2018-02-06 22:15:28 +0300

    Delete override

--- theme/overridetest/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$THEME->name = 'overridetest';
-$THEME->parents = array('standard', 'base', 'aardvark');
-
-$THEME->rendererfactory = 'theme_overridden_renderer_factory';
-
-$THEME->sheets = Array('overridetest');
-
